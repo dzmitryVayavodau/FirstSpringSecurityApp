683c85a2d1f0c37365ff8cee78bf3b038205e5ce
dzmitryVayavodau <dzmitry_vayavodau@senla.eu>
2024-11-15 11:25:59 +0300

    add PersonValidator

---
+++ src/main/java/com/voevodov/springcoure/FirstSecurityApp/util/PersonValidator.java
@@ -0,0 +1,41 @@
+package com.voevodov.springcoure.FirstSecurityApp.util;
+
+import com.voevodov.springcoure.FirstSecurityApp.model.Person;
+import com.voevodov.springcoure.FirstSecurityApp.services.PersonDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class PersonValidator implements Validator {
+
+    PersonDetailsService personDetailsService;
+
+    @Autowired
+    public PersonValidator(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Person.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        Person person = (Person) target;
+        System.out.println("person: " + person);
+
+        try {
+            personDetailsService.loadUserByUsername(person.getUserName());
+        }
+        catch (UsernameNotFoundException ignored) {
+            return;
+        }
+
+        errors.rejectValue("userName", "", "This userName already exists");
+
+    }
+}
